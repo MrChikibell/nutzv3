185e3d31edc95395986e04edfa22bb4b48099046
Hugo Nuñez Romante <hnromante@MacBook-Air-de-Hugo.local>
2018-05-01 13:06:21 -0300

    corregido el usuario y paciente modelos

--- NUTZ/paciente/models.py
+++ NUTZ/paciente/models.py
@@ -6,14 +6,13 @@ from django.dispatch import receiver
 # Create your models here.
 
 class Paciente(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='paciente')
     #Información personal - a llenar despues
     ocupacion = models.CharField(max_length=255)
     nacionalidad = models.CharField(max_length=100)
     observacion = models.TextField(max_length=5000)
     ultima_atencion = models.DateTimeField(null=True, blank=True)
-
+    
     #informacion nutricional
     peso = models.IntegerField(default=0)
 
@@ -21,18 +20,20 @@ class Paciente(models.Model):
     glicemia_mgdl = models.FloatField(null=True, blank=True)
 
     def __str__(self):
-        return self.user.rut
+        return self.user.email
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Paciente.objects.create(user=instance)
+        print("PACIENTE CREADO")
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
+    instance.paciente.get(pk=instance.pk).save()
+    # instance.paciente.all()[0].save()
 
 def update_profile(request, user_id):
     user = User.objects.get(pk=user_id)
-    user.profile.bio = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit...'
+    user.paciente.observacion = 'Objservacion desde modelo'
     user.save()
